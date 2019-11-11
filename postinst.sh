3708828ccd7492e50946954345d1f7c9fe5526d6
andresonsouza <andresonjazz@gmail.com>
2019-11-11 08:48:20 -0300

    Wine

--- postinst.sh
+++ postinst.sh
@@ -10,3 +10,11 @@ sudo apt-get install linux-image-liquorix-amd64 linux-headers-liquorix-amd64
 
 #ZRAM
 sudo apt install -y zram-config
+
+#Wine
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+sudo apt-key add winehq.key
+sudo apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/'
+sudo apt update
+sudo apt install --install-recommends winehq-staging
+sudo apt install winetricks
