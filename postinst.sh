8f69d7555e4db3665c22b16235fc1d785bedd974
andresonsouza <andresonjazz@gmail.com>
2019-11-11 08:46:38 -0300

    Kernel liquorix e zram

--- postinst.sh
+++ postinst.sh
@@ -3,3 +3,10 @@
 sudo apt update;sudo apt dist-upgrade -y
 sudo apt install -y deborphan
 deborphan | xargs sudo apt autoremove --purge -y
+
+#Kernel Liquorix
+sudo add-apt-repository ppa:damentz/liquorix && sudo apt-get update
+sudo apt-get install linux-image-liquorix-amd64 linux-headers-liquorix-amd64
+
+#ZRAM
+sudo apt install -y zram-config
